32239632a7efd3c9bd3d8698c102df9c715352a8
tyrannosaurus-lee <miaow106@gmail.com>
2021-11-03 02:02:08 +0900

    clean push

--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import router from './router'
 
 createApp(App).use(router).mount('#app')
 // 예시 : https://github.com/frontendfunn/vue-3-swiper-parallax-image-gallery
+import { Swiper, SwiperSlide } from 'swiper/vue';
